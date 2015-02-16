29cb2349eec167e86cab1582debdd83fb41d149b
Brandon Hays <brandon.hays@gmail.com>
2015-02-15 23:14:14 -0600

    move bootstrap to manual version for deploy

--- frontend/Brocfile.js
+++ frontend/Brocfile.js
@@ -2,7 +2,11 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  vendorFiles: {
+    'handlebars.js': null
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -16,6 +20,10 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+
 app.import("bower_components/pixelate/pixelate.js");
+app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
+app.import("bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss");
+
 
 module.exports = app.toTree();
